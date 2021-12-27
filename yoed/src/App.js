7eba1c8605d19e9427fbaa7f4df2be8176e81b23
yoed <yoed83@gmail.com>
2021-12-27 10:45:31 -0400

    box4 animation

--- yoed/src/App.js
+++ yoed/src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import Box1 from "./componets/Box1/Box1";
 import Box2 from "./componets/Box2/Box2";
 import Box3 from "./componets/Box3/Box3";
+import Box4 from "./componets/Box4/Box4";
 
 function App() {
   return (
     <div className="App">
       <Box3 />
+      <Box4 />
       <Box1 />
       <Box2 />
     </div>
