5c3270b8ff06e3b9037ee45817f3575fa70dafc5
yoed <yoed83@gmail.com>
2021-12-28 01:18:53 -0400

    add box5 animations practica

--- yoed/src/App.js
+++ yoed/src/App.js
@@ -3,12 +3,14 @@ import Box1 from "./componets/Box1/Box1";
 import Box2 from "./componets/Box2/Box2";
 import Box3 from "./componets/Box3/Box3";
 import Box4 from "./componets/Box4/Box4";
+import Box5 from "./componets/Box5/Box5";
 
 function App() {
   return (
     <div className="App">
       <Box3 />
       <Box4 />
+      <Box5 />
       <Box1 />
       <Box2 />
     </div>
