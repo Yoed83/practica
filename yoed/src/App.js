4a6ba6a9d317cc4d71cb63e7addc9383414690aa
yoed <yoed83@gmail.com>
2022-02-20 20:57:02 -0400

    practica

--- yoed/src/App.js
+++ yoed/src/App.js
@@ -1,3 +1,4 @@
+import react, { useEffect, useState } from "react";
 import "./App.css";
 import Footer from "./componets/Footer/Footer";
 import Navbar from "./componets/Navbar/Navbar";
@@ -6,11 +7,33 @@ import Home from "./pages";
 import Work from "./pages/work";
 import Skills from "./pages/skills";
 import Contact from "./pages/contact";
+import Dropdown from "./componets/Dropdown/Dropdown";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false);
+        console.log("i rezedddd");
+      }
+    };
+    window.addEventListener("resize", hideMenu);
+
+    return () => {
+      window.removeEventListener("resize", hideMenu);
+    };
+  });
+
   return (
     <>
-      <Navbar />
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/skills" element={<Skills />} />
--- yoed/src/componets/Navbar/Navbar.jsx
+++ yoed/src/componets/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { BiMenu } from "react-icons/bi";
 
-const Navbar = () => {
+const Navbar = ({ toggle }) => {
   return (
     <nav
       className="flex justify-between items-center h-6 bg-white text-black relative
@@ -12,7 +12,7 @@ const Navbar = () => {
       <Link to="/" className="pl-8">
         YOED
       </Link>
-      <div className="px-4 cursor-pointer md:hidden">
+      <div className="px-4 cursor-pointer md:hidden" onClick={toggle}>
         <BiMenu />
       </div>
       <div className="pr-8 md:block hidden">
